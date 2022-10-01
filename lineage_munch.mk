59b498cadd738e70620c8498cb1d2ee37b0de9ab
Souljacker1 <sameerpyo39@gmail.com>
2022-10-01 21:11:09 +0530

    Offical bringup for RD13

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from alioth device
+# Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := arrow_munch
+PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -26,3 +26,19 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    PRIVATE_BUILD_DESC="missi-user 12 SKQ1.211006.001 V13.0.5.0.SLMMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/munch/munch:12/RKQ1.211001.001/V13.0.5.0.SLMMIXM:user/release-keys
+
+# Input additional flags here.
+RICE_MAINTAINER := SAMMY
+RICE_OFFICIAL := true
+RICE_DEVICE := munch
+WITH_GMS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+SUSHI_BOOTANIMATION := 1440
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+#TARGET_PIXEL_CHARGE_ANIM := true
+#TARGET_SUPPORTS_NOW_PLAYING := true
+#USE_PIXEL_CHARGER_IMAGES := true
+#TARGET_BUILD_GRAPHENEOS_CAMERA := false
